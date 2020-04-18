af44a6b4376394a4b6e2d0e16377181f5d62b12b
Erkberg <erisch01@web.de>
2020-04-18 20:41:30 +0200

    basic candle mechanic

---
+++ CandleInTheRain/Assets/Scripts/Player/CandleParticle.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleParticle : MonoBehaviour
+{
+    public enum CandleState
+    {
+        Safe,
+        Decreasing,
+        Extinct
+    }
+
+    public float currentHealth = 1f;
+    public float healthDecreaseMultiplier = 1f;
+    public float healthIncreaseMultiplier = 0.33f;
+    public ParticleSystem flame;
+    public CandleState currentState = CandleState.Safe;
+
+    private float initialEmission;
+
+    private void Awake()
+    {
+        initialEmission = flame.emissionRate;
+    }
+
+    private void Update()
+    {
+        CheckState();
+
+        flame.emissionRate = initialEmission * currentHealth;
+    }
+
+    private void CheckState()
+    {
+        if(currentState == CandleState.Safe)
+        {
+            if (currentHealth < 1f)
+                currentHealth += Time.deltaTime * healthIncreaseMultiplier;
+
+            if (currentHealth > 1f)
+                currentHealth = 1f;
+        }
+        else if(currentState == CandleState.Decreasing)
+        {
+            if (currentHealth > 0f)
+                currentHealth -= Time.deltaTime * healthDecreaseMultiplier;
+
+            if (currentHealth <= 0f)
+            {
+                currentHealth = 0f;
+                SetState(CandleState.Extinct);
+            }                
+        }
+    }
+
+    public void SetState(CandleState state)
+    {
+        if(currentState != CandleState.Extinct)
+            currentState = state;
+    }
+        
+    public void LightAnew()
+    {
+        currentState = CandleState.Safe;
+    }
+}
--- CandleInTheRain/Assets/Scripts/Player/PlayerCandle.cs
+++ CandleInTheRain/Assets/Scripts/Player/PlayerCandle.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerCandle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool candleActive = true;
+    public Transform upperHand;
+    public CandleParticle candleParticle;
+
+    [Header("MoveSpeed")]
+    public float moveUpSpeedRandomizer = 0.5f;
+    public float moveUpSpeed = 0.01f;
+    public float moveDownSpeed = 0.1f;
+
+    [Header("Bounds")]
+    public float upperhandUpperY = 0.11f;
+    public float upperHandLowerY = -0.1f;
+    public float burnJumpOffset = 0.2f;
+
+    private void Update()
     {
-        
+        if (candleActive)
+            MoveHand();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveHand()
     {
-        
+        if(Input.GetMouseButton(1))
+        {
+            upperHand.position -= new Vector3(0f, moveDownSpeed * Time.deltaTime, 0f);
+        }
+        else
+        {
+            upperHand.position += new Vector3(0f, moveUpSpeed * Random.Range(1f - moveUpSpeedRandomizer, 1f + moveUpSpeedRandomizer) * Time.deltaTime, 0f);
+        }
+
+        if(upperHand.transform.localPosition.y > upperhandUpperY)
+        {
+            candleParticle.SetState(CandleParticle.CandleState.Decreasing);
+        }
+        else if(upperHand.transform.localPosition.y < upperHandLowerY)
+        {
+            upperHand.position += new Vector3(0f, burnJumpOffset, 0f);
+        }
+        else
+        {
+            candleParticle.SetState(CandleParticle.CandleState.Safe);
+        }
     }
 }
